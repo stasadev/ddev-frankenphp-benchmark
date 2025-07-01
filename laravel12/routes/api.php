0198bad3082154aea1f93a3b4c1bd51739989170
Stanislav Zhuk <stasadev@gmail.com>
2025-07-01 17:31:01 +0300

    Add endpoints from thecaliskan/laravel-benchmark

---
+++ laravel12/routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Services\HttpService;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/health-check', fn (Request $request) => Response::noContent())->name('health-check');
+Route::get('/static', fn (Request $request) => Response::json(['status' => true]))->name('static');
+Route::get('/http-request', fn (Request $request, HttpService $httpService) => Response::json(json_decode($httpService->get('http://whoami/api'))))->name('http-request');
